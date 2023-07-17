ff5374694de1e195070c79fb53c8cfed976c9b22
umeshsaravanan <umeshsaravanan13@gmail.com>
2023-07-17 22:30:57 +0530

    changed header arrow fn

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,72 +1,65 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-scroll'
 import "./Header.css"
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 
-
-
-class Header extends Component {
-    state = { clicked: false };
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
+const Header = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!click);
     }
-    handleLinks = () => {
-        this.setState({clicked: !this.state.clicked })
+    const handleLinks = () => {
+        setClick(!click);
     }
 
-    render() {
-        
-        return (
-            <div className="header">
+    return (
+        <div className="header">
 
-                <div className="navbarLeft">
-                    <div className='menuIcon' onClick={this.handleClick}>
-                        {this.state.clicked ? (<FaTimes size={20} style={{ color: 'white' }} />) : (<FaBars size={20} style={{ color: 'white' }} />)}
-                    </div>
-                    <h1>Smart<span> Work</span></h1>
+            <div className="navbarLeft">
+                <div className='menuIcon' onClick={handleClick}>
+                    {click ? (<FaTimes size={20} style={{ color: 'white' }} />) : (<FaBars size={20} style={{ color: 'white' }} />)}
                 </div>
-                <ul className={this.state.clicked ? "navbarRight active" : "navbarRight"}>
-                    <li>
-                        <Link onClick={this.handleLinks} to='about' smooth={true} duration={500} offset={-80}>
-                            <h4>About Me</h4>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link onClick={this.handleLinks} to='skills' smooth={true} duration={500} offset={-190}>
-                            <h4>Skills</h4>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link onClick={this.handleLinks} to='projects' smooth={true} duration={500} offset={-80}>
-                            <h4>Project</h4>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link onClick={this.handleLinks} to='exp' smooth={true} duration={500} offset={-80}>
-                            <h4>Experience</h4>
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link onClick={this.handleLinks} to='contact' smooth={true} duration={500} offset={-80}>
-                            <h4>Contact</h4>
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link className='hide' onClick={this.handleLinks} to='joinme' smooth={true} duration={500} offset={-80}>
-                            <h4>Join with Me</h4>
-                        </Link>
-                    </li>
-
-                </ul>
-
+                <h1>Smart<span> Work</span></h1>
             </div>
+            <ul className={click ? "navbarRight active" : "navbarRight"}>
+                <li>
+                    <Link onClick={handleLinks} to='about' smooth={true} duration={500} offset={-80}>
+                        <h4>About Me</h4>
+                    </Link>
+                </li>
+                <li>
+                    <Link onClick={handleLinks} to='skills' smooth={true} duration={500} offset={-190}>
+                        <h4>Skills</h4>
+                    </Link>
+                </li>
+                <li>
+                    <Link onClick={handleLinks} to='projects' smooth={true} duration={500} offset={-80}>
+                        <h4>Project</h4>
+                    </Link>
+                </li>
+                <li>
+                    <Link onClick={handleLinks} to='exp' smooth={true} duration={500} offset={-80}>
+                        <h4>Experience</h4>
+                    </Link>
+                </li>
 
-        )
+                <li>
+                    <Link onClick={handleLinks} to='contact' smooth={true} duration={500} offset={-80}>
+                        <h4>Contact</h4>
+                    </Link>
+                </li>
 
-    }
+                <li>
+                    <Link className='hide' onClick={handleLinks} to='joinme' smooth={true} duration={500} offset={-80}>
+                        <h4>Join with Me</h4>
+                    </Link>
+                </li>
+
+            </ul>
+
+        </div>
+    )
 }
 
 export default Header;
