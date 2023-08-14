bf9971ccfe1de31ec0070f4917eb5016fc569372
umeshsaravanan <umeshsaravanan13@gmail.com>
2023-08-14 16:07:30 +0530

    links

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -35,11 +35,18 @@ const Project = ({img,title,desc,link}) => {
             <h2>{(title ==="React js")? "React js Projects" : "Hmtl & CSS Web Pages"}</h2>
             <p>
                 {(title ==="React js")? <>
-                <p style={{marginBottom : '15px'}}>Here is my Todo_list Project build using React js and Firebase</p>
+                <h2>Todo List:</h2>
+                <p style={{marginBottom : '15px'}}>A minimalist Todo List using React js and Firebase</p>
                 <a href='https://todo-list-853a5.web.app/' target='blank'>Check it out</a>
+                <h4>GitHub Repository: <a href='https://github.com/umeshsaravanan/Todo_List' target='blank'>umeshsaravanan/Todo_List</a> </h4>
+                <h2>Feedback:</h2>
+                <p style={{marginBottom : '15px'}}>A ReactJS-based web app for students to provide feedback on faculty</p>
+                <h4>GitHub Repository: <a href='https://github.com/umeshsaravanan/Feedback' target='blank'>umeshsaravanan/Feedback</a> </h4>
                 </> : <>
+                <h2>Revive:</h2>
                 <p style={{marginBottom : '15px'}}>It's a simple web page build using HTML, CSS and JS </p>
                 <a href='https://umeshsaravanan.github.io/Revive-CSE/' target='blank'>Click to see</a>
+                <h4>GitHub Repository: <a href='https://github.com/umeshsaravanan/Revive-CSE' target='blank'>umeshsaravanan/Revive</a> </h4>
                 </>}
             </p>
             <button className="close-modal" onClick={toggleModal}>
